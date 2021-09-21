3c35e63d60e44223389e4fac21801df28027a124
gergocsontos <gergo.csontos.123@gmail.com>
2021-09-21 11:36:24 +0200

    add starwars-themed png

--- src/main/java/com/codecool/dungeoncrawl/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/Tiles.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Tiles {
     public static int TILE_WIDTH = 32;
 
-    private static Image tileset = new Image("/tiles.png", 543 * 2, 543 * 2, true, false);
+    private static Image tileset = new Image("/dungeon-crawl.png", 543 * 2, 543 * 2, true, false);
     private static Map<String, Tile> tileMap = new HashMap<>();
     public static class Tile {
         public final int x, y, w, h;
@@ -23,11 +23,11 @@ public class Tiles {
     }
 
     static {
-        tileMap.put("empty", new Tile(0, 0));
-        tileMap.put("wall", new Tile(10, 17));
-        tileMap.put("floor", new Tile(2, 0));
-        tileMap.put("player", new Tile(27, 0));
-        tileMap.put("skeleton", new Tile(29, 6));
+        tileMap.put("empty", new Tile(1, 0));
+        tileMap.put("wall", new Tile(2, 0));
+        tileMap.put("floor", new Tile(3, 0));
+        tileMap.put("player", new Tile(3, 0));
+        tileMap.put("skeleton", new Tile(4, 0));
     }
 
     public static void drawTile(GraphicsContext context, Drawable d, int x, int y) {
