a8835f5164227a215df710bc74e80e535792a56e
Csaba Pistyur <csaba.pistyur@gmail.com>
2021-09-23 17:59:12 +0200

    give healing variable to bluemilk

--- src/main/java/com/codecool/dungeoncrawl/logic/items/BlueMilk.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/BlueMilk.java
@@ -4,6 +4,7 @@ import com.codecool.dungeoncrawl.logic.Cell;
 
 public class BlueMilk extends Item{
     private static final String className = "Blue Milk";
+    private static final int healing = 20;
     public BlueMilk(Cell cell, String tileName) {
         super(cell, tileName);
         this.name = className;
@@ -12,5 +13,9 @@ public class BlueMilk extends Item{
 
     public static String getClassName() {
         return className;
+    }
+
+    public static int getHealing() {
+        return healing;
     }
 }
