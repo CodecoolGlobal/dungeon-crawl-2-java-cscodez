15611ea95131d65c62950945d009e0467308f51f
Csaba Pistyur <csaba.pistyur@gmail.com>
2021-10-05 16:02:08 +0200

    Refactor display inventory method, outsource remove inventory. SavingMenu method is also added.

--- src/main/java/com/codecool/dungeoncrawl/util/BuildUI.java
+++ src/main/java/com/codecool/dungeoncrawl/util/BuildUI.java
@@ -1,39 +1,58 @@
 package com.codecool.dungeoncrawl.util;
 
+import com.codecool.dungeoncrawl.dao.GameDatabaseManager;
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.GameMap;
 import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.logic.items.BlueMilk;
+import com.codecool.dungeoncrawl.logic.items.Item;
 import com.codecool.dungeoncrawl.logic.items.Weapon;
+import com.codecool.dungeoncrawl.model.PlayerModel;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class BuildUI {
+
+    GameDatabaseManager manager = new GameDatabaseManager();
+    HashMap<Label, Button> whatsOnUi;
+    GridPane ui;
+    Label inventoryLabel;
 
     public void inventoryDisplayer(GridPane ui, GameMap map, Label healthLabel) {
         int itemCol = 0;
         int buttonCol = 1;
         int row = 3;
+        this.ui = ui;
+        //Label inventoryLabel = new Label();
         Player player = map.getPlayer();
         HashMap<String, Integer> hashMap= player.getInventory();
-        HashMap<Label, Button> whatsOnUi= new HashMap<>();
+        this.whatsOnUi= new HashMap<>();
+        if (hashMap.keySet().size() == 0) {
+            this.inventoryLabel = new Label();
+            inventoryLabel.setText("Inventory\nis empty!");
+            this.ui.add(inventoryLabel, 0, 1);
+            this.whatsOnUi.put(inventoryLabel, null);
+        }
         for (String item : hashMap.keySet()) {
             if (!item.equals("Lightsaber")) {
                 StringBuilder displayString = new StringBuilder();
-                Label inventoryLabel = new Label();
                 Button useButton = new Button("use");
+                this.inventoryLabel = new Label();
                 displayString.append(item);
                 displayString.append(" : ");
                 displayString.append(hashMap.get(item));
                 displayString.append("\n");
                 inventoryLabel.setText(displayString.toString());
-                ui.add(inventoryLabel, itemCol, row);
-                ui.add(useButton, buttonCol, row);
+                this.ui.add(inventoryLabel, itemCol, row);
+                this.ui.add(useButton, buttonCol, row);
                 whatsOnUi.put(inventoryLabel, useButton);
                 row++;
                 if (item.equals(BlueMilk.getClassName())) {
@@ -48,18 +67,13 @@ public class BuildUI {
                 }
             }
         }
+    }
 
-        Button exitButton = new Button("exit");
-        ui.add(exitButton, 0, row);
-        EventHandler<ActionEvent> exitHandler = e -> {
-            for (Label label:whatsOnUi.keySet()) {
-                ui.getChildren().remove(label);
-                ui.getChildren().remove(whatsOnUi.get(label));
-            }
-            ui.getChildren().remove(exitButton);
-        };
-        exitButton.setOnAction(exitHandler);
-
+    public void removeInventory() {
+        for (Label label:this.whatsOnUi.keySet()) {
+            this.ui.getChildren().remove(label);
+            this.ui.getChildren().remove(whatsOnUi.get(label));
+        }
     }
 
     public void pickUpButtonHandler(GridPane ui, Cell cell, GameMap map) {
@@ -87,5 +101,39 @@ public class BuildUI {
         ui.add(yesButton, 0, 1);
         ui.add(noButton, 1, 1);
 
+    }
+
+    public void savingMenu(GridPane ui, GameMap map) {
+        Button cancelButton = new Button("Cancel");
+        Button saveAndExitButton = new Button("Save & Exit");
+        Button exitWithoutSaveButton = new Button("Exit");
+
+        ArrayList<Button> saveMenuButtons = new ArrayList<>();
+
+        ui.add(saveAndExitButton, 0, 1);
+        saveMenuButtons.add(saveAndExitButton);
+        ui.add(exitWithoutSaveButton, 0, 2);
+        saveMenuButtons.add(exitWithoutSaveButton);
+        ui.add(cancelButton, 0, 4);
+        saveMenuButtons.add(cancelButton);
+
+        EventHandler<ActionEvent> saveAndExitButtonHandler = e -> {
+            //manager.savePlayer(map.getPlayer());
+            System.exit(0);
+
+        };
+
+        EventHandler<ActionEvent> exitButtonHandler = e -> {
+            System.exit(0);
+        };
+
+        EventHandler<ActionEvent> cancelHandler = e -> {
+            for (Button button : saveMenuButtons) {
+                ui.getChildren().remove(button);
+            }
+        };
+        cancelButton.setOnAction(cancelHandler);
+        saveAndExitButton.setOnAction(saveAndExitButtonHandler);
+        exitWithoutSaveButton.setOnAction(exitButtonHandler);
     }
 }
