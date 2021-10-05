57b2cca5a18dfef7db39125fd33224ecec53a27f
Csaba Pistyur <csaba.pistyur@gmail.com>
2021-10-05 16:00:18 +0200

    Refactor action for case I, to close inventory for repeated key I press

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -36,6 +36,7 @@ public class Main extends Application {
     GraphicsContext context = canvas.getGraphicsContext2D();
     Label healthLabel = new Label();
     GameDatabaseManager dbManager;
+    boolean isInventoryActive = false;
 
 
     public static void main(String[] args) {
@@ -95,7 +96,17 @@ public class Main extends Application {
                     map.getPlayer().move(1, 0);
                     break;
                 case I:
-                    uiBuilder.inventoryDisplayer(ui, map, healthLabel);
+                    if (!isInventoryActive) {
+                        uiBuilder.inventoryDisplayer(ui, map, healthLabel);
+                        isInventoryActive = true;
+                    } else {
+                        uiBuilder.removeInventory();
+                        isInventoryActive = !isInventoryActive;
+                    }
+                    break;
+                case S:
+                    uiBuilder.savingMenu(ui, map);
+
 
             }
         }
