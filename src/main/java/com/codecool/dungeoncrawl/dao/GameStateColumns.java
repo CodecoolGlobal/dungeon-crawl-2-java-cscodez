27ae5f4026662178ad175edaf7af35bb139924ae
Papp Bence <papp.bence15@gmail.com>
2021-10-06 14:03:40 +0200

    Rename too long names

---
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateColumns.java
@@ -0,0 +1,20 @@
+package com.codecool.dungeoncrawl.dao;
+
+public enum GameStateColumns {
+    ID("id"),
+    CURRENT_MAP("current_map"),
+    SAVED_AT("saved_at"),
+    PLAYER_ID("player_id"),
+    NAME_OF_SAVE("name_of_save");
+
+
+    private final String name;
+
+    GameStateColumns(String gameStateColumnName) {
+        name = gameStateColumnName;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
