863f3a082118df738ded0f958028c03f0887382a
Csaba Pistyur <csaba.pistyur@gmail.com>
2021-10-07 17:46:40 +0200

    Refactor serialize and deserialize methods

--- src/main/java/com/codecool/dungeoncrawl/util/Serialization.java
+++ src/main/java/com/codecool/dungeoncrawl/util/Serialization.java
@@ -1,16 +1,27 @@
 package com.codecool.dungeoncrawl.util;
 
+import com.codecool.dungeoncrawl.logic.actors.Actor;
+import com.codecool.dungeoncrawl.logic.items.Item;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Serialization {
 
-    private static final Gson gson = new Gson();
-
+    private static Gson gson = new Gson();
+    private static final GsonBuilder gsonBuilder = new GsonBuilder()
+;
     public static String serialize(Object object) {
+        gsonBuilder.registerTypeAdapter(Item.class, new InterfaceAdapter<Item>());
+        gsonBuilder.registerTypeAdapter(Actor.class, new InterfaceAdapter<Actor>());
+        gson = gsonBuilder.create();
         return gson.toJson(object);
     }
 
     public static <T> Object deserialize(String json, Class<T> classofT) {
+        gsonBuilder.registerTypeAdapter(Item.class, new InterfaceAdapter<Item>());
+        gsonBuilder.registerTypeAdapter(Actor.class, new InterfaceAdapter<Actor>());
+        gson = gsonBuilder.create();
+
         return gson.fromJson(json, classofT);
     }
 }
